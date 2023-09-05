c0b9a9c53ffcc12e9e74f68caae58f092e4c5ccd
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-09-05 22:32:38 +0300

    Form in Login component is created

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,31 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
 function Login() {
-  return <div className="Login"></div>;
+  const { register, handleSubmit } = useForm({
+    defaultValues: { username: "", password: "" },
+  });
+
+  const onSubmit = (data) => {
+    console.log("form data: ", data);
+  };
+
+  return (
+    <div className="Login">
+      <h1>LOGIN</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>
+          USERNAME
+          <input type="text" name="username" {...register("username")} />
+        </label>
+        <label>
+          PASSWORD
+          <input type="password" name="password" {...register("password")} />
+        </label>
+        <button type="submit">SUBMIT</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
